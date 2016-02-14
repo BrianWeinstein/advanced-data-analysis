4429b0c18e227c1aa00ce6b0fd40d2f83eb859ac
BrianWeinstein <bweinstein02@gmail.com>
2016-02-14 16:29:45 -0500

    started problem 3

--- homework_03/hw03.R
+++ homework_03/hw03.R
@@ -33,6 +33,52 @@ rm(list = ls()) # clear working environment
 
 # Problem 3: Ramsey 5.19 #######################################################################
 
+# input data
+cavityData <- data.frame(n=c(127, 44, 24, 41, 18, 16, 11, 7, 6),
+                         logMean=c(7.347, 7.368, 7.418, 7.487, 7.563, 7.568, 8.214, 8.272, 8.297),
+                         logSampleSd=c(0.4979, 0.4235, 0.3955, 0.3183, 0.3111, 0.4649, 0.2963, 0.3242, 0.5842))
+
+# Part a ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+# pooled estimate of variance
+pooledVar <- cavityData %>%
+  mutate(numeratorTerm=((n-1)*logSampleSd^2),
+         denominatorTerm=(n-1)) %>%
+  summarize(numerator=sum(numeratorTerm),
+            denomintor=sum(denominatorTerm))
+pooledVar <- pooledVar$numerator / pooledVar$denomintor
+pooledVar
+
+# Part b ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+# sum of squares within
+ssw <- cavityData %>%
+  mutate(term=((n-1)*logSampleSd)) %>%
+  summarize(sumOfSquaresWithin=sum(term))
+ssw
+
+
+# total sum of squares
+sst <- (sum(cavityData$n) - 1) * 0.4962
+sst
+
+
+# sum of squares between
+sst - ssw
+
+# p-value of F-statistic
+pf(q=6.5300, df1=8, df2=285, lower.tail=FALSE)
+
+
+
+# Part c ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+# compute the overall mean using a weight average of the group means
+overallLogMean <- sum(cavityData$n * cavityData$logMean) / sum(cavityData$n)
+overallLogMean
+
+# recomopute the sum of squares between
+sum((cavityData$n * (cavityData$logMean)^2 )) - (294 * overallLogMean^2)
 
 
 rm(list = ls()) # clear working environment
