1d69a80053624e03dc865a4dc7abce35ef178fbe
BrianWeinstein <bweinstein02@gmail.com>
2016-02-20 17:43:40 -0500

    problem 1

--- homework_04/hw04.R
+++ homework_04/hw04.R
@@ -9,16 +9,33 @@
 setwd("~/Documents/advanced-data-analysis/homework_04")
 
 # load packages
-# library(dplyr)
+library(dplyr)
 library(Sleuth3) # Data sets from Ramsey and Schafer's "Statistical Sleuth (3rd ed)"
-# library(ggplot2); theme_set(theme_bw())
+library(ggplot2); theme_set(theme_bw())
 # library(data.table)
 
 
 
 # Problem 1: Ramsey 5.23  #######################################################################
 
+# load data
+trexData <- Sleuth3::ex0523 %>%
+  mutate(BoneNumber=as.factor(as.integer(gsub("Bone", "", Bone))))
 
+# boxplots of oxygen composition in each bone
+ggplot(trexData, aes(x=BoneNumber, y=Oxygen)) +
+  geom_violin(alpha=0.15) +
+  geom_boxplot() +
+  geom_point(alpha=0.15)+
+  labs(y="Oxygen Isotopic Composition\n(per mil deviations from SMOW)", x="Bone Number")
+ggsave(filename="writeup/1.png", width=6.125, height=3.5, units="in")
+
+# use a one way ANOVA F-test
+anovaTable <- anova(lm(Oxygen~Bone, data=trexData)); anovaTable
+
+# compute the total sum of squares and the total degrees of freedom
+sum(anovaTable$'Sum Sq')
+sum(anovaTable$Df)
 
 rm(list = ls()) # clear working environment
 
