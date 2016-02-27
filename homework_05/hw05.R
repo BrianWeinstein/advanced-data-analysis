45cfc61880c4494924dd0c91df1af6239b228db0
BrianWeinstein <bweinstein02@gmail.com>
2016-02-27 16:33:52 -0500

    set up templates for hw5

---
+++ homework_05/hw05.R
@@ -0,0 +1,65 @@
+
+################################################################################
+# Brian Weinstein - bmw2148
+# STAT W4201 001
+# Homework 5
+# 2016-03-02
+
+# set working directory
+setwd("~/Documents/advanced-data-analysis/homework_05")
+
+# load packages
+library(Sleuth3) # Data sets from Ramsey and Schafer's "Statistical Sleuth (3rd ed)"
+# library(ggplot2); theme_set(theme_bw())
+# library(dplyr)
+# library(scales)
+# library(gmodels)
+# library(agricolae)
+
+
+
+# Problem 1: Ramsey 7.18  #######################################################################
+
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 2: Ramsey 7.24  #######################################################################
+
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 3: Ramsey 7.28  #######################################################################
+
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 4: Ramsey 8.17  #######################################################################
+
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 5: Ramsey 8.20  #######################################################################
+
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 6: Ramsey 9.12  #######################################################################
+
+
+
+rm(list = ls()) # clear working environment
