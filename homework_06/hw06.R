6872b5d748a6e5a5cba0cf3aac98070ced7b1d3e
BrianWeinstein <bweinstein02@gmail.com>
2016-03-05 10:48:42 -0500

    setup templates for hw6

---
+++ homework_06/hw06.R
@@ -0,0 +1,67 @@
+
+################################################################################
+# Brian Weinstein - bmw2148
+# STAT W4201 001
+# Homework 6
+# 2016-03-09
+
+# set working directory
+setwd("~/Documents/advanced-data-analysis/homework_06")
+
+# prevent R from printing large numbers in scientific notation
+# options(scipen=5)
+
+# load packages
+library(Sleuth3) # Data sets from Ramsey and Schafer's "Statistical Sleuth (3rd ed)"
+# library(ggplot2); theme_set(theme_bw())
+# library(gridExtra)
+# library(GGally)
+# library(dplyr)
+
+
+
+# Problem 1: Ramsey 9.14  #######################################################################
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 2: Ramsey 9.16  #######################################################################
+
+# Part a ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 3: Ramsey 9.18  #######################################################################
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 4: Ramsey 9.20  #######################################################################
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 5: Ramsey 10.19  #######################################################################
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 6: Ramsey 10.28  #######################################################################
+
+
+rm(list = ls()) # clear working environment
+
+
+
