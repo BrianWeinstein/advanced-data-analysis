5d27df57bbcec973c73a540ee803cd3362b6c9f4
BrianWeinstein <bweinstein02@gmail.com>
2016-04-10 14:50:13 -0400

    set up templates for hw9

---
+++ homework_09/hw09.R
@@ -0,0 +1,45 @@
+
+################################################################################
+# Brian Weinstein - bmw2148
+# STAT W4201 001
+# Homework 9
+# 2016-04-13
+
+# set working directory
+setwd("~/Documents/advanced-data-analysis/homework_09")
+
+# prevent R from printing large numbers in scientific notation
+options(scipen=5)
+
+# load packages
+library(Sleuth3) # Data sets from Ramsey and Schafer's "Statistical Sleuth (3rd ed)"
+# library(MASS)
+# library(dplyr)
+# library(ggplot2); theme_set(theme_bw())
+# library(GGally)
+# library(leaps)
+# library(tidyr)
+# library(ggrepel)
+# library(gridExtra)
+
+
+
+# Problem 1: Ramsey 20.12 #######################################################################
+
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 2: Ramsey 21.16 #######################################################################
+
+
+
+rm(list = ls()) # clear working environment
+
+
+
+# Problem 3 #######################################################################
+
+# no code needed
